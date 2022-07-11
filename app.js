a1bfaab167abbf0c8e91393f36d7e279c37b3c8b
dbarrera8912 <barrerad2020@gmail.com>
2022-07-11 10:58:21 -0300

    suvo al repo estilo

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const port = 3030;
 
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));
+
 app.use(express.static('public'));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, "views", "home.html")));
 
